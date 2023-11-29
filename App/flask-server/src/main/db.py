93d6c537b6856af4b4244caf645d9363775855df
AnuragDixitt <anshiva.dixit76@gmail.com>
2023-11-30 01:24:08 +0530

    Update DB

--- App/flask-server/src/main/db.py
+++ App/flask-server/src/main/db.py
@@ -4,11 +4,11 @@ from pymongo import MongoClient
 # client = MongoClient('mongodb://admin:password@localhost:8100') 
 
 # For Docker compose up use this
-# client = MongoClient('mongodb://admin:password@mongodb') 
+client = MongoClient('mongodb://admin:password@mongodb') 
 
 # Trying for kubernetes
 # client = MongoClient('mongodb://admin:password@mongodb') 
-client = MongoClient("mongodb://mongodb-service:27017")
+# client = MongoClient("mongodb://mongo:27017")
 
 database = client['SPF'] 
     
